24ae57fb1aea387e6c14c2ba12eda6604ff20d03
bumble-bee9579 <9579bhushan@gmail.com>
2025-02-08 07:10:56 +0000

    datatypes

--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,1 +1,19 @@
-console.log("hiii mr bhushan ")
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword ="123456"
+accountCity ="jaipur"
+let accountState;
+
+//accountId = 2  //not allowed 
+
+accountEmail = "hc@hc.com"
+accountPassword ="21212121"
+accountCity ="bengluru"
+console.log(accountId);
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
